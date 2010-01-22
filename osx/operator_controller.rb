3ab9809a1f52a0d4d735d6867bcc4f5aaccc8480
norrby <norrby@lysator.liu.se>
2010-01-22 20:33:04 +0100

    ysing sub-project locally instead of global libraries

--- osx/operator_controller.rb
+++ osx/operator_controller.rb
@@ -1,12 +1,22 @@
 class OperatorController < NSViewController
-	attr_writer :ar_slider, :tl_slider
-	attr_accessor :where
-	
-	def tl_changed(sender)
-		puts "tl is"
-	end
-	
-	def initWithNibName(bundle)
-		puts "hahahah"
-	end
+  attr_writer :button1, :button2
+  attr_writer :where
+  
+  def tl_changed(sender)
+    puts "tl is"
+  end
+  
+  def awakeFromNib
+    @where.addSubview(view)
+    puts "fasandeR2"
+    #view.setFrameOrigin(NSPoint.new(0.0, 0.0))
+  end
+
+  def sendEvent(event)
+    puts event
+  end
+
+  def dragImage(at, offset, event,pasteboard, source, slideBack)
+    puts "dragging"
+  end
 end
