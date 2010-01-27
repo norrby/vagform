3582ea013cf6ed871b9e569be1e0183908a28703
norrby <norrby@lysator.liu.se>
2010-01-27 22:53:13 +0100

    moving ar capability for view

--- osx/waveform_view.rb
+++ osx/waveform_view.rb
@@ -1,42 +1,21 @@
 # -*- coding: undecided -*-
 class WaveformView < NSCustomView
-
   attr_writer :controller
   attr_writer :operator
 
-  class FakeOperator
-    attr_reader :tl, :dt1, :ar, :d1r, :d2r, :sl, :rr
-    def initialize
-      @tl = 0
-      @dt1 = 4
-      @ar = 20
-      @d1r = 20
-      @d2r = 25
-      @sl = 5
-      @rr = 5
-    end
-    
-  end
-
   def awakeFromNib()
-    @op = FakeOperator.new
   end
   
   def mouseDown(event)
-#    pa = NSPoint.new(10, 10)
-#    @point = convertPoint(evmouent.locationInWindow, fromView:nil)
-#    puts "#{@point.x}#{@point.y}"
-#    puts "x: #{event.locationInWindow.x}"
-#    puts "y: #{event.locationInWindow.y}"
-#    setNeedsDisplay(true)
+    pa = NSPoint.new(10, 10)
+    @point = convertPoint(event.locationInWindow, fromView:nil)
+    puts "#{@point.x}#{@point.y}"
+    puts "x: #{event.locationInWindow.x}"
+    puts "y: #{event.locationInWindow.y}"
   end
 
   def mouseDragged(event)
   end
-
-  def drawRect(rect)
-  end
-
   def isFlipped
     false #true
   end
@@ -45,18 +24,18 @@ class WaveformView < NSCustomView
     path = NSBezierPath.bezierPath
     path.setLineWidth(4)
     NSBezierPath.setDefaultLineCapStyle(NSRoundLineCapStyle)
-    start_point = NSPoint.new(1, 10)
-    frame_height = frame.size.height
-    top = NSPoint.new(20, frame_height)
-    sustain_start = NSPoint.new(30, 10)
-    sustain_end = NSPoint.new(40, 10)
+    ar_origin = @operator.frame.origin
+    ar_size = @operator.bounds.size
+    ar = NSPoint.new(ar_origin.x + ar_size.width/2,
+                     ar_origin.y + ar_size.height/2, )
+    start_point = NSPoint.new(ar_size.width/2, 0)
     path.moveToPoint(start_point)
-    path.lineToPoint(top)
-    path.lineToPoint(sustain_start)
-    path.lineToPoint(sustain_end)
+    path.lineToPoint(ar)
     #path.closePath
     NSColor.greenColor.set 
     path.stroke
+    setNeedsDisplay(true)
+
 #    start_point = @point || NSPoint.new(1, 1);
 #    end_point = NSPoint.new(50, 50)
 # NSPoint.new(60, 60);
