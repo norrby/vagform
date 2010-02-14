03bcb1bc903d8b2806fe793e9377f69cb4fb119d
norrby <norrby@lysator.liu.se>
2010-02-14 19:52:06 +0100

    use a HUD window for modifying operator parameters

--- src/yfb01/operator.rb
+++ src/yfb01/operator.rb
@@ -10,14 +10,14 @@ class Operator
     :level_scaling_depth => Memory[0, 15, 0x02, 0xF0],
     :adjust_for_tl => Memory[0, 15, 0x02, 0x0F],
     :scaling_internal_bit1 => Memory[0, 1, 0x03, 0x80],
-    :detune => Memory[0, 7, 0x03, 0x70],
-    :detune_frequency => Memory[0, 15, 0x03, 0x0F],
+    :dt1 => Memory[0, 7, 0x03, 0x70],
+    :frequency_multiplier => Memory[0, 15, 0x03, 0x0F],
     :rate_scaling_depth => Memory[0, 3, 0x04, 0xC0],
     :ar => Memory[0, 31, 0x04, 0x1F],
     :carrier => Memory[0, 1, 0x05, 0x80],
     :ar_velocity_sensitivity => Memory[0, 3, 0x05, 0x60],
     :d1r => Memory[0, 31, 0x05, 0x1F],
-    :inharmonic_frequency => Memory[0, 3, 0x06, 0xC0],
+    :dt2 => Memory[0, 3, 0x06, 0xC0],
     :d2r => Memory[0, 31, 0x06, 0x1F],
     :sl => Memory[0, 15, 0x07, 0xF0],
     :rr => Memory[0, 15, 0x07, 0x0F]
@@ -31,7 +31,40 @@ class Operator
   end
 
   def level_scaling_type
-    return scaling_internal_bit0 + (scaling_internal_bit1 << 1)
+    self.scaling_internal_bit0 + (self.scaling_internal_bit1 << 1)
+  end
+
+  def detune_fine
+    return 0 if dt1 == 0
+    self.dt1 - 4
+  end
+
+  def detune_fine=(value)
+    self.dt1 = value + 4
+  end
+
+  def min_detune_fine
+    self.min_dt1 - 4
+  end
+
+  def max_detune_fine
+    self.max_dt1 - 4
+  end
+
+  def total_level
+    self.max_tl - self.tl
+  end
+
+  def total_level=(value)
+    self.tl = self.max_tl - value
+  end
+
+  def max_total_level
+    self.max_tl
+  end
+
+  def min_total_level
+    self.min_tl
   end
 
   def level_scaling_type=(value)
