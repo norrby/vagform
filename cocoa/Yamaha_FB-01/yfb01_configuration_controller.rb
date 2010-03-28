3fec6957de3e621f65d2edd2b2210e1318d4280d
norrby <norrby@lysator.liu.se>
2010-03-28 19:13:53 +0200

    Working selection of instruments

--- cocoa/Yamaha_FB-01/yfb01_configuration_controller.rb
+++ cocoa/Yamaha_FB-01/yfb01_configuration_controller.rb
@@ -2,10 +2,15 @@ require 'Yamaha_FB-01/yfb01_memory_controller'
 
 class Yfb01ConfigurationController
   include Yfb01MemoryController
+  attr_accessor :instrument_controllers
 
   def initialize(model)
     puts "initializing model with instance of #{model.class}"
     @model = model
+    instr_no = 0
+    @instrument_controllers = model.instruments.collect do |instr|
+      Yfb01InstrumentController.new(instr, instr_no += 1)
+    end
   end
 
   def model
--- cocoa/Yamaha_FB-01/yfb01_instrument_bay.rb
+++ cocoa/Yamaha_FB-01/yfb01_instrument_bay.rb
@@ -7,9 +7,37 @@
 class Yfb01InstrumentBay < NSViewController
   attr_writer :parent_view
   attr_accessor :document
+  attr_accessor :instrument_controllers
+
+  def observeValueForKeyPath(keyPath, ofObject:object,
+                             change:change, context:context)
+    @instrument_controllers.arrangedObjects.each_with_index do |instr, idx|
+      puts "replacing instrument_controller"
+      @small_views[idx].instrument_controller = instr
+    end
+  end
 
   def awakeFromNib
     puts "instrument bay awoke"
-    @parent_view.addSubview(view) if view
+    return unless @instrument_controllers
+    @instrument_controllers.addObserver(self, forKeyPath:"arrangedObjects",
+                                        options:0, context:nil)
+    prev_view |= nil
+    @small_views ||= []
+    @instrument_controllers.arrangedObjects.each do |instr|
+      small = Yfb01InstrumentSmallViewController.alloc
+      @small_views << small
+      small.instrument_controller = instr
+      small.instrument_controllers = @instrument_controllers
+      small.initWithNibName("Yfb01InstrumentSmall", bundle:nil)
+      if prev_view
+        height = small.view.frame.size.height
+        ypos = prev_view.frame.origin.y
+        origin = NSPoint.new(small.view.frame.origin.x, ypos + height)
+        small.view.setFrameOrigin(origin)
+      end
+      prev_view = small.view
+      @parent_view.addSubview(small.view)
+    end
   end
 end
